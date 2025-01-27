350a2111dd757544d52c6e570eea81c958b91cf0
Женя <instant99@mail.ru>
2025-01-27 20:39:30 +0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -45,7 +45,7 @@ export default defineAppConfig({
       title: 'На этой странице',
       links: [{
         title: 'Купить подписку',
-        icon: ':smart-icon{name="/public/tg.svg"}',
+        icon: ':smart-icon{name="/tg.svg"}',
         to: 'https://t.me/vpnghostbot',
         target: '_blank',
       }],
