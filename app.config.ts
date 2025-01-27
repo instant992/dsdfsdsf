0c97012178a46bcc4801935a5b75c31fbe8498e6
Женя <instant99@mail.ru>
2025-01-27 20:11:35 +0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -45,7 +45,7 @@ export default defineAppConfig({
       title: 'На этой странице',
       links: [{
         title: 'Купить подписку',
-        icon: 'lucide:telegram',
+        icon: 'lucide:star',
         to: 'https://t.me/vpnghostbot',
         target: '_blank',
       }],
